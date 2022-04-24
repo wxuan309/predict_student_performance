57fd97b3828f912baedd038fe6b78d8d399dc3f4
RohanJoshi28 <78099546+RohanJoshi28@users.noreply.github.com>
2022-04-24 00:00:35 -0400

    add omni exercise: display a graph when the user types in input (frequency of words)

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 # import requirements needed
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for
 from utils import get_base_url
+from collections import Counter
+import matplotlib.pyplot as plt
 
 # setup the webserver
 # port may need to be changed if there are multiple flask servers running on same server
@@ -17,6 +19,25 @@ else:
 @app.route(f'{base_url}')
 def home():
     return render_template('index.html')
+
+@app.route('/form')
+def get_paragraph():
+    return render_template('form.html')
+
+@app.route('/form', methods=['POST'])
+def get_graph():
+    text = request.form['text_box']
+    processed_text = text.split(" ")
+    counted = Counter(processed_text)
+
+    x = [tag for tag, count in counted.most_common(20)]
+    y = [count for tag, count in counted.most_common(20)]
+
+    fig = plt.figure()
+    plt.bar(x, y, color='crimson')
+    plt.title("Frequencies of words in entered text")
+    fig.savefig('static/saved_img.png')
+    return render_template('image.html')
 
 # define additional routes here
 # for example:
