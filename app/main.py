260f6497f363f0dc6139cd3d2f79aa8cc934289d
RohanJoshi28 <78099546+RohanJoshi28@users.noreply.github.com>
2022-04-28 12:16:28 -0400

    add face detection functionality to base omni repo

--- app/main.py
+++ app/main.py
@@ -1,11 +1,16 @@
 # import requirements needed
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from utils import get_base_url
+import cv2
+import numpy as np
 
 # setup the webserver
 # port may need to be changed if there are multiple flask servers running on same server
 port = 12345
 base_url = get_base_url(port)
+
+#get the cascade classifier from the cv2 filepath
+faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
 # if the base url is not empty, then the server is running in development, and we need to specify the static folder so that the static files are served
 if base_url == '/':
@@ -17,6 +22,40 @@ else:
 @app.route(f'{base_url}')
 def home():
     return render_template('index.html')
+
+@app.route(f'{base_url}/upload')
+def upload():
+    return render_template('image_upload.html')
+
+def detect_faces(image):
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    # Detect faces in the image
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30, 30),
+        flags = cv2.CASCADE_SCALE_IMAGE
+    )
+
+    return faces
+
+@app.route(f'{base_url}/display', methods=["POST"])
+def display():
+    if request.method=="POST":
+        uploaded_file = request.files['file']
+        npimg = np.fromfile(uploaded_file, np.uint8)
+        image = cv2.imdecode(npimg, cv2.IMREAD_COLOR)
+
+        faces = detect_faces(image)
+
+        # Draw a rectangle around the faces
+        for (x, y, w, h) in faces:
+            cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+        cv2.imwrite('static/saved_image.png', image)
+        return render_template('image_display.html')
 
 # define additional routes here
 # for example:
